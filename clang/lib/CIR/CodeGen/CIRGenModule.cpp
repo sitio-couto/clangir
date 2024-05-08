7344459b73a8c23b49584083ab0c5c6da37db10d
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2024-05-08 20:41:12 -0300

    [WIP] Set cir.triple attr during CIRGen

--- clang/lib/CIR/CodeGen/CIRGenModule.cpp
+++ clang/lib/CIR/CodeGen/CIRGenModule.cpp
@@ -183,6 +183,7 @@ CIRGenModule::CIRGenModule(mlir::MLIRContext &context,
                      mlir::cir::SignedOverflowBehaviorAttr::get(&context, sob));
   theModule->setAttr(
       "cir.lang", mlir::cir::LangAttr::get(&context, getCIRSourceLanguage()));
+  theModule->setAttr("cir.triple", builder.getStringAttr(getTriple().str()));
   // Set the module name to be the name of the main file. TranslationUnitDecl
   // often contains invalid source locations and isn't a reliable source for the
   // module location.
